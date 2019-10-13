35de685d99e0587da53a4e0a97852f4bebc157ae
Janae Hall <janaeelisa@gmail.com>
2019-10-13 08:57:53 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,4 @@
 require_relative 'animals'
 
 class Cat < Animal
-  def initialize(name, owner)
-    super
-    owner.cats << self
-  end
-
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,4 @@
 require_relative 'animals'
 
 class Dog < Animal
-
-  def initialize(name, owner)
-    super
-    owner.dogs << self
-  end
 end
